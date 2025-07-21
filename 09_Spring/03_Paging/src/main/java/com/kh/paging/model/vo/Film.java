430f092b0c4809948e34a9cabe09aefbe843edc5
Jiyeon232 <wldus23299@gmail.com>
2025-07-21 15:12:42 +0900

    250721

---
+++ 09_Spring/03_Paging/src/main/java/com/kh/paging/model/vo/Film.java
@@ -0,0 +1,14 @@
+package com.kh.paging.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor @AllArgsConstructor @Data
+public class Film {
+
+	private int filmId;
+	private String title;
+	private String description;
+	
+}
